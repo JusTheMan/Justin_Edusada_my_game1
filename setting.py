4add9f2394b4544064c7ea2015c806d9d74943e0
JusTheMan <126901439+JusTheMan@users.noreply.github.com>
2023-03-09 12:02:07 -0800

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,17 @@
+#File created by: Justin Edusada
+
+#screen dimension
+WIDTH = 800
+HEIGHT = 600
+
+# game settings:
+FPS = 30
+RUNNING = True
+
+#playr attributes 
+PLAYER_ACC = 2 
+PLAYER_FRICTION = -0.12
+
+#colors
+BLACK = (0,0,0)
+BLUE = ( 0,0, 225)
